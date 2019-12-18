d01c77e810580a83df1ff65f142987e3154a907f
jenniew <jenniewang123@gmail.com>
2019-12-18 13:20:39 -0800

    update run script name

--- run_train_mem_model_zoo.sh
+++
@@ -1,5 +0,0 @@
-export OMP_NUM_THREADS=10
-${ANALYTICS_ZOO_HOME}/bin/spark-submit-python-with-zoo.sh \
- --master local[4] \
- --driver-memory 20g \
- train_mem_model_zoo.py /home/nvkvs/data/aggregated_5min_scaled.csv 128 10 /home/nvkvs/ARMemNet-jennie/model
