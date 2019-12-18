7d1d868add47231cbb1c3d783eea78e89d499e93
jenniew <jenniewang123@gmail.com>
2019-12-18 01:24:13 -0800

    update core-num and thread-num

--- train_mem_model_zoo.py
+++ train_mem_model_zoo.py
@@ -4,6 +4,7 @@ from bigdl.optim.optimizer import *
 from data_utils import load_agg_selected_data_mem_train
 from AR_mem.config import Config
 from AR_mem.model import Model
+from time import time
 import tensorflow as tf
 from zoo.common import set_core_number
 
@@ -14,6 +15,14 @@ if __name__ == "__main__":
     batch_size = int(sys.argv[2])
     num_epochs = int(sys.argv[3])
     model_dir = sys.argv[4]
+    if len(sys.argv) > 5:
+        core_num = sys.argv[5]
+    else:
+        core_num = 4
+    if len(sys.argv) > 6:
+        thread_num = sys.argv[6]
+    else:
+        thread_num = 10
 
     config = Config()
     config.data_path = data_path
@@ -21,6 +30,7 @@ if __name__ == "__main__":
 
     # init or get SparkContext
     sc = init_nncontext()
+    set_core_number(core_num)
 
     # create train data
     train_x, dev_x, test_x, train_y, dev_y, test_y, train_m, dev_m, test_m, test_dt = \
@@ -38,7 +48,15 @@ if __name__ == "__main__":
     model = Model(config, dataset.tensors[0], dataset.tensors[1], dataset.tensors[2])
     optimizer = TFOptimizer.from_loss(model.loss, Adam(config.lr),
                                       metrics={"rse": model.rse, "smape": model.smape, "mae": model.mae},
-                                      model_dir=model_dir)
+                                      model_dir=model_dir,
+                                      session_config=tf.ConfigProto(inter_op_parallelism_threads=1,
+                                                                    intra_op_parallelism_threads=thread_num)
+                                      )
 
+    start_time = time()
     optimizer.optimize(end_trigger=MaxEpoch(num_epochs))
+    end_time = time()
+
+    print("Elapsed training time {} secs".format(end_time - start_time))
+
 
