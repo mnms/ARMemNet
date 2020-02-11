ebc8eae18bd4beb2ef3e53dc24a071e4e20e0a1f
jenniew <jenniewang123@gmail.com>
2020-02-12 02:01:00 +0800

    update for yarn script

--- run_train_mem_model_zoo_yarn.sh
+++ run_train_mem_model_zoo_yarn.sh
@@ -3,6 +3,11 @@ export PYSPARK_PYTHON=python3
 export OMP_NUM_THREADS=10
 export HADOOP_CONF_DIR=${HADOOP_HOME}/etc/hadoop
 
+if [ -f train.zip ]
+  then rm train.zip
+fi
+zip -r train.zip *.py */*.py
+
 ${ANALYTICS_ZOO_HOME}/bin/spark-submit-python-with-zoo.sh \
  --master yarn \
  --deploy-mode client \
@@ -10,4 +15,5 @@ ${ANALYTICS_ZOO_HOME}/bin/spark-submit-python-with-zoo.sh \
  --executor-memory 6g \
  --executor-cores 2 \
  --num-executors 1 \
+ --py-files train.zip \
  train_mem_model_zoo.py hdfs://localhost:9000/skt 128 10 /Users/wangjiao/git/ARMemNet-jennie/model
