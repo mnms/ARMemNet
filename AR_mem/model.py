29f8328e22b2cd3d3de1a81ab8c4ef434ac85aa4
GitHub Action <reserve.dev@gmail.com>
2021-03-25 19:19:01 +0900

    changed to train with tf2 on single gpu

--- AR_mem/model.py
+++ AR_mem/model.py
@@ -1,177 +1,92 @@
 import tensorflow as tf
-from tensorflow.contrib import layers
-import os
+from tensorflow.keras import regularizers, initializers
+from tensorflow.keras.layers import *
+
+#customAutoRegressive
+class customAutoRegressive(tf.keras.layers.Layer):
+    def __init__(self, ar_lambda, x_len, nfeatures):
+        super(customAutoRegressive, self).__init__()
+        self.ar_labda = ar_lambda
+        self.x_len = x_len
+        self.nfeatures = nfeatures
+        self.initializer_glorot_uniform=tf.keras.initializers.GlorotUniform()
+        self.initializer_zeros = tf.keras.initializers.Zeros()
+
+    def build(self, input_shape) :
+        self.w = self.add_weight(name='w', shape=[self.x_len, self.nfeatures], dtype='float32',\
+                initializer=self.initializer_glorot_uniform)
+        self.b = self.add_weight(name='bias', shape=[self.nfeatures], dtype='float32',\
+                initializer=self.initializer_zeros)
+
+    @tf.function(experimental_compile=True)
+    def call(self, inputs):
+        _w = tf.expand_dims(self.w, axis=0)
+        weighted = tf.math.reduce_sum(inputs * _w, axis=1) + self.b
+        ar_loss = self.ar_labda * tf.math.reduce_sum(tf.math.square(self.w))
+        return weighted, ar_loss
+
+#customAttention
+class customAttention(tf.keras.layers.Layer):
+    def __init__(self, attention_size, mstep, nfeatures, regularizer):
+        super(customAttention, self).__init__()
+        self.attention_size = attention_size
+        self.mstep = mstep
+        self.nfeatures = nfeatures
+        self.initializer_glorot_uniform = tf.keras.initializers.GlorotUniform()
+        self.regularizer = regularizer
+        self.Query = Dense(self.attention_size, \
+                activation=None, use_bias=False, kernel_initializer=self.initializer_glorot_uniform, \
+                kernel_regularizer = self.regularizer)
+        self.Key_dense = Dense(self.attention_size, \
+                activation=None,use_bias=False, kernel_initializer=self.initializer_glorot_uniform,\
+                kernel_regularizer=self.regularizer)
+        self.Projection_act = Activation(tf.nn.tanh)
+        self.SimMatrix_dense = Dense(1, activation=None, \
+                use_bias=False, kernel_initializer='glorot_uniform', kernel_regularizer=self.regularizer)
+        self.SimMatrix_softmax = Softmax(1)
+
+    def build(self, input_shape):
+        self.attention_bias = self.add_weight(name='attention_bias', shape=[self.attention_size], dtype='float32',\
+                initializer=self.initializer_glorot_uniform)
+        
+    @tf.function(experimental_compile=True)
+    def call(self, inputs): #inputs[0]=inputs inputs[1]=memories
+        x = tf.expand_dims(inputs[0], axis=1)
+        query = self.Query(x)
+        key = tf.reshape(inputs[1], shape=[-1, self.mstep, self.nfeatures])
+        key = self.Key_dense(key)
+        projection = query + key + self.attention_bias
+        projection = self.Projection_act(projection)
+        sim_matrix = self.SimMatrix_dense(projection)
+        sim_matrix = self.SimMatrix_softmax(sim_matrix)
+        sim_matrix = tf.transpose(sim_matrix, [0,2,1])
+        context = tf.matmul(sim_matrix, key)
+        context = tf.squeeze(context, axis=1)
+        return context
 
 # AR_memory
-class Model(object):
+class Model(tf.keras.Model):
     def __init__(self, config):
+        super(Model, self).__init__()
         self.config = config
-        self.global_step = tf.Variable(0, trainable=False, name="global_step")
-        self.regularizer = layers.l2_regularizer(self.config.l2_lambda)
-        self.sess = None
-        self.saver = None
-        self._build_model()
-        
-    def _build_model(self):
-        self.add_placeholder()
-        # get auto-regression
-        # input_ar : [b, nf]
-        with tf.variable_scope("inputs"):
-            input_ar, ar_loss = self.auto_regressive(self.input_x, self.config.ar_lambda, self.config.x_len, scope="inputs_")
-            
-        with tf.variable_scope("memories"):    
-            # memory : [b, (n+1)*m, nf] -> [b*m, n+1, nf]
-            memories = tf.concat(tf.split(self.memories, self.config.msteps, axis=1), axis=0)
-            # memories = tf.reshape(self.memories, shape=[-1, self.config.nsteps+1, self.config.nfeatures])
-            # memory_ar : [b*m, nf]
-            memory_ar, ar_loss_ = self.auto_regressive(memories, self.config.ar_lambda, self.config.x_len+1, scope="memories")
-        
-        # context: [b, nf]
-        context = self.attention(input_ar, memory_ar)
-        linear_inputs = tf.concat([input_ar, context], axis=1) # [b, 2nf] 
-        self.predictions = tf.layers.dense(linear_inputs, self.config.nfeatures, activation=tf.nn.tanh,
-                                           kernel_initializer=layers.xavier_initializer(), use_bias=False)
-        #self.predictions = input_ar
-        self.loss = tf.losses.mean_squared_error(labels=self.targets, predictions=self.predictions)
-        #self.loss = tf.reduce_mean(tf.abs(self.targets - self.predictions))
-        
-        # metric
-        error = tf.reduce_sum((self.targets - self.predictions)**2) ** 0.5
-        denom = tf.reduce_sum((self.targets - tf.reduce_mean(self.targets))**2) ** 0.5
-        self.rse = error / denom
-        self.mape = tf.reduce_mean(tf.abs((self.targets - self.predictions)/self.targets))
-        self.smape = tf.reduce_mean(2*tf.abs(self.targets-self.predictions)/(tf.abs(self.targets)+tf.abs(self.predictions)))
-        self.mae = tf.reduce_mean(tf.abs(self.targets - self.predictions))
-        '''
-        if self.config.l2_lambda > 0:
-            reg_vars = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
-            reg_term = layers.apply_regularization(self.regularizer, reg_vars)
-            self.loss += reg_term
-        '''    
-        # self.loss = self.loss + ar_loss + ar_loss_
-        
-        self.add_train_op()
-        self.initialize_session()
+        self.regularizer = regularizers.l2(self.config.l2_lambda)
+        self.global_step = tf.Variable(0, trainable=False, name='global_step')
+        self.auto_regressive_input  = customAutoRegressive(self.config.ar_lambda, self.config.x_len, self.config.nfeatures)
+        self.auto_regressive_memory = customAutoRegressive(self.config.ar_lambda, self.config.x_len+1, self.config.nfeatures)
+        self.attention = customAttention(self.config.attention_size, self.config.msteps, self.config.nfeatures, self.regularizer)
+        self.prediction = Dense(self.config.nfeatures, \
+                activation=tf.nn.tanh, use_bias=False, kernel_initializer='glorot_uniform')
 
-    def add_placeholder(self):
-        self.input_x = tf.placeholder(shape=[None, self.config.nsteps, self.config.nfeatures],dtype=tf.float32, name="x")
-        self.targets = tf.placeholder(shape=[None, self.config.nfeatures], dtype=tf.float32, name="targets")
-        self.memories = tf.placeholder(shape=[None, (self.config.nsteps+1) * self.config.msteps, self.config.nfeatures], dtype=tf.float32,
-                                       name="memories")
-        # self.targets = tf.placeholder(shape=[None], dtype=tf.int32, name="targets")
-        self.dropout = tf.placeholder(dtype=tf.float32, name="dropout")
-        
-    def auto_regressive(self, inputs, ar_lambda, x_len, scope, reuse=False):
-        # y_t,d = sum_i(w_i * y_i,d) + b_d
-        w = tf.get_variable(shape=[x_len, self.config.nfeatures],
-                            initializer=layers.xavier_initializer(),
-                            name="w")
-        bias = tf.get_variable(shape=[self.config.nfeatures],
-                    initializer=tf.zeros_initializer(),
-                    name="bias")     
-        w_ = tf.expand_dims(w, axis=0)
-        # weighted: [b, nf]
-        weighted = tf.reduce_sum(inputs * w_, axis=1) + bias
-        
-        ar_loss = ar_lambda * tf.reduce_sum(tf.square(w))
-        return weighted, ar_loss
-    
-    def attention(self, inputs, memories):
-        # use MLP to compute attention score
-        # given input, attend memories(m1, m2, m3, mnstep)
-        
-        # inputs : [b, nf] -> [b, 1, nf]
-        query = tf.expand_dims(inputs, axis=1)
-        # [b, 1, attention_size]
-        query = tf.layers.dense(query, self.config.attention_size,
-                               activation=None,use_bias=False,
-                               kernel_initializer=layers.xavier_initializer(),
-                               kernel_regularizer=self.regularizer)
-        
-        # memories : [b*m, nf] -> [b, m, nf]  
-        key = tf.reshape(memories, shape=[-1, self.config.msteps, self.config.nfeatures])
-        # [b, m, attention_size]
-        key = tf.layers.dense(key, self.config.attention_size,
-                              activation=None,use_bias=False,
-                               kernel_initializer=layers.xavier_initializer(),
-                               kernel_regularizer=self.regularizer)
-        
-        bias = tf.get_variable(shape=[self.config.attention_size],
-                              initializer=tf.zeros_initializer(),
-                              name="attention_bias")
-        
-        # projection : [b, m, attention_size]
-        projection = tf.nn.tanh(query + key + bias)
-        # sim_matrix : [b, m, 1]
-        sim_matrix = tf.layers.dense(projection, 1, activation=None)
-        sim_matrix = tf.nn.softmax(sim_matrix, 1)
-        # context : [b, 1, nf] -> [b, nf]
-        context = tf.matmul(tf.transpose(sim_matrix, [0,2,1]), key)
-        context = tf.squeeze(context, axis=1)
+    @tf.function(experimental_compile=True)
+    def call(self, inputs, training=True): #inputs[0]=inputs inputs[1]=memories
+        input_ar, ar_loss_input = self.auto_regressive_input(inputs[0])
+        memories = tf.concat(tf.split(inputs[1], self.config.msteps, axis=1), axis=0)
+        memory_ar, ar_loss_memories = self.auto_regressive_memory(memories)
+        context = self.attention([input_ar, memory_ar])
+        linear_inputs = tf.concat([input_ar, context], axis=1)
+        predictions = self.prediction(linear_inputs)
+        return predictions
 
-        return context
-    
- 
-    def add_train_op(self):
-        opt = tf.train.AdamOptimizer(self.config.lr)
-        vars = tf.trainable_variables()
-        grads = tf.gradients(self.loss, vars)
-        clipped_grads,_ = tf.clip_by_global_norm(grads,self.config.clip)
-        self.train_op = opt.apply_gradients(zip(grads,vars), global_step=self.global_step)
-        
-    def initialize_session(self):
-        """Defines self.sess and initialize the variables"""
-        if not self.config.allow_gpu:
-            config = tf.ConfigProto(device_count={'GPU': 0})
-            os.environ["CUDA_VISIBLE_DEVICES"] = "-1"            
-        else:   
-            os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-            config = tf.ConfigProto(allow_soft_placement=True)
-            config.gpu_options.allow_growth = True
-            config.gpu_options.per_process_gpu_memory_fraction = 0.9
-        self.sess = tf.Session(config=config)
-        self.sess.run(tf.global_variables_initializer())
-        self.saver = tf.train.Saver()
-        
-    def save_session(self, model_name):
-        """Saves session = weights"""
-        self.saver.save(self.sess, model_name)
-        
-    def restore_session(self, dir_model):
-        """Reload weights into session
-        Args:
-            sess:tf.Session()
-            dir_model: dir with weights
-        """
-        self.saver.restore(self.sess, tf.train.latest_checkpoint(dir_model))
-    
-    def train(self, input_x, mem, targets):
-        feed_dict = {
-            self.input_x: input_x,
-            self.memories: mem,
-            self.targets: targets,
-            self.dropout: self.config.dropout
-        }
-    
-        output_feed = [self.train_op, self.loss, self.rse, self.smape, self.mae, self.global_step]
-        _, loss, rse, smape, mae, step = self.sess.run(output_feed, feed_dict)
-      
-        return loss, rse, smape, mae, step
-        
-        
-    def eval(self, input_x, mem, targets):
-        feed_dict = {
-            self.input_x: input_x,
-            self.memories: mem,
-            self.targets: targets,
-            self.dropout: 1.0
-        }
-        output_feed = [self.predictions, self.loss, self.rse, self.smape, self.mae]
-        pred, loss, rse, smape, mae = self.sess.run(output_feed, feed_dict)
-
-        return pred, loss, rse, smape, mae
-
-    
 if __name__ == "__main__":
     from config import Config
     config = Config()
--- train_mem_model.py
+++ train_mem_model.py
@@ -1,22 +1,26 @@
 import os
-from utils import get_logger, make_date_dir
-from data_utils import load_agg_selected_data_mem, batch_loader
 import numpy as np
 from time import time
+import tensorflow as tf
+from utils import get_logger, make_date_dir
+from data_utils import load_agg_selected_data_mem, batch_loader
 from AR_mem.config import Config
 from AR_mem.model import Model
+from AR_mem.losses import RSE, SMAPE
+from tensorflow.keras.losses import MSE, MAE
 
 
 def main():
     config = Config()
-        
+
     logger, log_dir = get_logger(os.path.join(config.model, "logs/"))
     logger.info("=======Model Configuration=======")
     logger.info(config.desc)
     logger.info("=================================")
-    
-    try:       
-        train_x, dev_x, test_x, train_y, dev_y, test_y, train_m, dev_m, test_m, test_dt = load_agg_selected_data_mem(data_path=config.data_path, \
+
+    try:
+        # prepare train, evaluation data
+        train_x, dev_x, _, train_y, dev_y, _, train_m, dev_m, _, _ = load_agg_selected_data_mem(data_path=config.data_path, \
             x_len=config.x_len, \
             y_len=config.y_len, \
             foresight=config.foresight, \
@@ -24,53 +28,75 @@ def main():
             dev_ratio=config.dev_ratio, \
             test_len=config.test_len, \
             seed=config.seed)
-                    
-        model = Model(config)
         train_data = list(zip(train_x, train_m, train_y))
 
-        no_improv = 0 
+        # prepare training
+        logger.info("prepare training...")
+        no_improv = 0
         best_loss = 100
+        model = Model(config)
+        optimizer = tf.keras.optimizers.Adam(learning_rate=config.lr)
         model_dir = make_date_dir(os.path.join(config.model, 'model_save/'))
-        logger.info("Start training")
-        
+
+        MSE = tf.keras.losses.MeanSquaredError()
+        MAE = tf.keras.losses.MeanAbsoluteError()
+
+        # training
+        logger.info("start training...")
         start_time = time()
+
         for i in range(config.num_epochs):
             train_batches = batch_loader(train_data, config.batch_size)
-            epoch = i+1
-            
-            for batch in train_batches:
-                batch_x, batch_m, batch_y = zip(*batch)
-                loss, rse, smape, mae, step = model.train(batch_x, batch_m, batch_y)
+            cur_epoch = i + 1
 
-                if step % 100 == 0:
-                    logger.info("epoch: %d, step: %d, loss: %.4f, rse: %.4f, smape: %.4f, mae: %.4f" %
-                                (epoch, step, loss, rse, smape, mae))
-                    
-            # dev score for each epoch (no mini batch)
-            _, dev_loss, dev_rse, dev_smape, dev_mae = model.eval(dev_x, dev_m, dev_y)
-        
-            if dev_loss < best_loss:
-                best_loss = dev_loss
+            for step, batch in enumerate(train_batches):
+                batch_x, batch_m, batch_y = zip(*batch)
+
+                with tf.GradientTape() as tape:
+                    preds = model([batch_x, batch_m], training=True)
+                    loss_mse = MSE(batch_y, preds)
+                    loss_rse = RSE(batch_y, preds)
+                    loss_smape = SMAPE(batch_y, preds)
+                    loss_mae = MAE(batch_y, preds)
+
+                    grads = tape.gradient(loss_mse, model.trainable_variables)
+                    clipped_grads = tf.clip_by_global_norm(grads, config.clip)
+                    optimizer.apply_gradients(zip(grads, model.trainable_variables))
+
+                if step % 50 == 0:
+                    logger.info("[T] epoch: {}, step: {}, loss: {:.4f}, rse: {:.4f}, smape: {:.4f}, mae: {:.4f}".format( \
+                        cur_epoch, step, loss_mse, loss_rse, loss_smape, loss_mae))
+
+            # evaluate per epoch
+            preds_dev = model([dev_x, dev_m], training=False)
+            dev_mse = MSE(dev_y, preds_dev)
+            dev_rse = RSE(dev_y, preds_dev)
+            dev_smape = SMAPE(dev_y, preds_dev)
+            dev_mae = MAE(dev_y, preds_dev)
+
+            if dev_mse < best_loss:
+                best_loss = dev_mse
                 no_improv = 0
-                logger.info("New score! : dev_loss: %.4f, dev_rse: %.4f, dev_smape: %.4f, dev_mae: %.4f" % 
-                            (dev_loss, dev_rse, dev_smape, dev_mae))
+                logger.info("New score!: dev_loss: {:.8f}, dev_rse: {:.4f}, dev_smape: {:.4f}, dev_mae: {:.4f}".format( \
+                    dev_mse, dev_rse, dev_smape, dev_mae))
                 logger.info("Saving model at {}".format(model_dir))
-                model.save_session(os.path.join(model_dir, config.model))
-            else: 
-                no_improv += 1
-                if no_improv == config.nepoch_no_improv:
-                    logger.info("No improvement for %d epochs" % no_improv)
+                # tf.keras.models.save_model(model, model_dir, save_format="tf")
+                model.save(model_dir)
+            else:
+                no_improv = no_improv + 1
+                if no_improv >= config.nepoch_no_improv:
+                    logger.info("No improvement for {} epochs".format(no_improv))
                     break
-        
+
+        #
         elapsed = time() - start_time
         # generating results (no mini batch)
         logger.info("Saving model at {}".format(model_dir))
         logger.info("Elapsed training time {0:0.2f} mins".format(elapsed/60))
         logger.info("Training finished, exit program")
-        
+
     except:
         logger.exception("ERROR")
-    
+
 if __name__ == "__main__":
     main()
-    
